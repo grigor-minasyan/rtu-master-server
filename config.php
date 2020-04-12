06b7ca65fec42685166be7f2fa5ea56729f2fdbc
Grigor Minasyan <grigorminasyan1998@gmail.com>
2020-04-12 01:10:43 -0700

    added the config file too, it is local anyway

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'phpcode');
+define('DB_PASSWORD', 'Dpstelecom1998-=');
+define('DB_NAME', 'project6db');
+
+/* Attempt to connect to MySQL database */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
